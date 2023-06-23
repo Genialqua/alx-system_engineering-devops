fa44afa3752958dceb113a2bc18a46889b7aec8e
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-06-23 23:46:53 +0100

    Edited installing a package with puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,5 +8,5 @@ package { 'flask':
 # This script contains the command
 
 exec { 'pip3':
-  command => '/usr/bin/python3 sudo pip3 install -v 2.1.0 flask'
+  command => '/usr/bin/python3 sudo pip3 install flask == 2.1.0'
 }
