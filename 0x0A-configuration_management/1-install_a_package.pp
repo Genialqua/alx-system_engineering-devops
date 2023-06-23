c3c20b5944c406ea8dbb105ec2c488287a106f60
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-06-23 23:54:15 +0100

    Edited installing a package with puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
 # This manifest installs a package
 
 package { 'flask':
-  ensure  => '2.1.0',
-  require => Exec['pip3']
-}
-
-# This script contains the command
-
-exec { 'pip3':
-  command => '/usr/bin/python3 sudo pip3 install flask == 2.1.0'
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
