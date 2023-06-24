436c167c1347df5f4cea9d09fc1d90851655b812
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2023-06-24 17:14:20 +0100

    This script terminates a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,13 +1,13 @@
 # This is to list all running processes
 
 exec {'ps':
-  command => 'ps -ef | grep killmenow'
+  command  => 'ps -ef | grep killmenow'
 }
 
 # This is to terminate a process
 
 exec {'kill':
   require => Exec['ps'],
-  command => 'pkill -f killmenow'
+  command => 'pkill killmenow'
 }
 
